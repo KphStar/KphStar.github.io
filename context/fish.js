f9bcd8bcb5b7d6b4c7b68802ba2085434c7af1b1
KphStar <lerockandroy@gmail.com>
2025-06-19 13:46:20 -0700

    change in vec

--- context/fish.js
+++ context/fish.js
@@ -275,7 +275,7 @@ shader.vertexShader = shader.vertexShader.replace(
   `#include <begin_vertex>`,
   `#include <begin_vertex>
 
-  vec3 pos = position;
+  vec3 pos = position * uScale;
 
   float wStep = 1. / uTextureSize.x;
   float hWStep = wStep * 0.5;
